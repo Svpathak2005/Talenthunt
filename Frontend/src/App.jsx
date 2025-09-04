04cb9f5c32b019b57e66ff2601c1a08ba812d34d
Svpathak2005 <svpathak5@gmail.com>
2025-09-04 21:04:45 +0530

    landing page fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import StudentDashboard from "./pages/StudentDashboard";
@@ -12,7 +13,8 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/mentor-dashboard" element={<MentorDashboard />} />
